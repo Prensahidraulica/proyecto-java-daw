a65ff5e34fef08cca2aa4e78be0d54c8b9a2c6fb
Elena <villaelenacusac@gmail.com>
2025-10-16 17:19:30 +0200

    Añadir segundo mensaje sobre gestión de versiones

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionando con Git");
+        System.out.println("La gestión de versiones es una competencia clave.");
     }
 }
