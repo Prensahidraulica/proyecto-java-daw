27044ef567cd5fd086d4183034e6dc24041ffda6
Elena <elenalapalmera17@gmail.com>
2025-10-20 09:57:24 +0200

    Estoy en la rama main

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionando con Git");
         System.out.println("La gestión de versiones es una competencia clave.");
         System.out.println("-------------------------------------------------------------");
+        System.out.println("Estoy en la rama main");
     }
 }
