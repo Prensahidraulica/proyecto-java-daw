7adcf2a87866d87a5eda5a8f0f6fc989d184decb
Elena <elenalapalmera17@gmail.com>
2025-10-20 09:58:27 +0200

    Dos sout con una separación entre frases

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,9 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionando con Git");
         System.out.println("La gestión de versiones es una competencia clave.");
+        System.out.println();
         System.out.println("-------------------------------------------------------------");
+        System.out.println();
         System.out.println("Estoy en la rama main");
     }
 }
